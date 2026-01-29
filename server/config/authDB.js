2558fe32630251fb346e164b52b2eea7a9429e19
s225153983 <s225153983@deakin.edu.au>
2026-01-29 19:20:59 +1000

    Revert "Add Docker configuration and update auth DB config"
    This reverts commit fb9b7fb7bb895b9b0800a8d768684348cce23882.

--- server/config/authDB.js
+++ server/config/authDB.js
@@ -2,13 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    if (!process.env.MONGO_URI) {
-      throw new Error("MONGO_URI not set");
-    }
-    await mongoose.connect(process.env.MONGO_URI);
+await mongoose.connect(
+  "mongodb+srv://recipeUser:recipe123@cluster0.oczyss3.mongodb.net/?appName=Cluster0"
+);
     console.log("MongoDB connected");
-  } catch (err) {
-    console.error(err.message);
+  } catch (error) {
+    console.error("DB connection failed:", error);
     process.exit(1);
   }
 };
